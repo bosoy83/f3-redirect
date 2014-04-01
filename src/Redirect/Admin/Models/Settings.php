8ded5d3082206d0c697e741882589216ab62b54c
Lukas Polak <lpolak@dioscouri.com>
2014-03-31 23:11:02 -0400

    Ups, forgot to clean up

--- src/Redirect/Admin/Models/Settings.php
+++ src/Redirect/Admin/Models/Settings.php
@@ -3,25 +3,5 @@ namespace Redirect\Admin\Models;
 
 class Settings extends \Dsc\Mongo\Collections\Settings
 {
-    protected $type = 'redirect.settings';
-    
-    public function prefab( $source=array(), $options=array() )
-    {
-        $prefab = new \Redirect\Admin\Models\Prefabs\Settings($source, $options);
-    
-        return $prefab;
-    }
-    
-    /**
-     * An alias for the save command, used only for creating a new object
-     *
-     * @param array $values
-     * @param array $options
-     */
-    public function create( $values, $options=array() )
-    {
-        $values = array_merge( $this->prefab()->cast(), $values );
-    
-        return $this->save( $values, $options );
-    }
+    protected $__type = 'redirect.settings';
 }
