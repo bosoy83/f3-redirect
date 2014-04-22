d276bc69e3c999ce932431ad020724489e33d8ff
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-04-21 21:33:57 -0400

    only some formatting changes

--- src/Redirect/Routes.php
+++ src/Redirect/Routes.php
@@ -1,32 +1,22 @@
 <?php
-
 namespace Redirect;
 
-/**
- * Group class is used to keep track of a group of routes with similar aspects (the same controller, the same f3-app and etc)
- * 
- * @author Lukas Polak
- */
-class Routes extends \Dsc\Routes\Group{
-	
-	
-	function __construct(){
-		parent::__construct();
-	}
-	
-	/**
-	 * Initializes all routes for this group
-	 * NOTE: This method should be overriden by every group
-	 */
-	public function initialize(){
-		$this->setDefaults(
-				array(
-					'namespace' => '\Redirect\Admin\Controllers',
-					'url_prefix' => '/admin/redirect'
-				)
-		);
-		
-		$this->addSettingsRoutes();
-		$this->addCrudGroup( 'Routes', 'Route', array('datatable_links' => true));
-	}
+class Routes extends \Dsc\Routes\Group
+{
+    /**
+     * Initializes all routes for this group
+     * NOTE: This method should be overriden by every group
+     */
+    public function initialize()
+    {
+        $this->setDefaults(array(
+            'namespace' => '\Redirect\Admin\Controllers',
+            'url_prefix' => '/admin/redirect'
+        ));
+        
+        $this->addSettingsRoutes();
+        $this->addCrudGroup('Routes', 'Route', array(
+            'datatable_links' => true
+        ));
+    }
 }
