22193201ef13a454f68c0875955521d860a975c9
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-05-05 23:14:41 -0400

    Revert "Replacing ./ with /"
    This reverts commit a1f88317c92bfc3a762a2927cfd103115327c359.

--- src/Redirect/Admin/Views/routes/list.php
+++ src/Redirect/Admin/Views/routes/list.php
@@ -11,7 +11,7 @@
 	<div class="col-xs-12 col-sm-5 col-md-5 col-lg-8">
         <ul id="sparks" class="list-actions list-unstyled list-inline">
             <li>
-                <a class="btn btn-default" href="/admin/redirect/route/create">Add New</a>
+                <a class="btn btn-default" href="./admin/redirect/route/create">Add New</a>
             </li>
         </ul>            	
 	</div>
@@ -19,7 +19,7 @@
 
 <div class="row">
     <div class="col-md-9">
-        <form id="routes" class="searchForm" action="/admin/redirect/routes" method="post">
+        <form id="routes" class="searchForm" action="./admin/redirect/routes" method="post">
     		<?php echo $this->renderLayout('Redirect/Admin/Views::routes/list_datatable.php'); ?>    
         </form>
     </div>
