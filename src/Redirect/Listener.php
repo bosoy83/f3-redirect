54257fde00caa4a4e78279e1e3e7da0b5ed48b8c
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-05-17 10:06:43 -0400

    updated menu item adder

--- src/Redirect/Listener.php
+++ src/Redirect/Listener.php
@@ -1,32 +1,39 @@
-<?php 
+<?php
 namespace Redirect;
 
-class Listener extends \Prefab 
+class Listener extends \Prefab
 {
-    public function onSystemRebuildMenu( $event )
-    {
-        if ($model = $event->getArgument('model')) 
-        {
-        	$root = $event->getArgument( 'root' );
-        	$redirect = clone $model;
 
-        	$redirect->insert(
-        			array(
-        					'type'	=> 'admin.nav',
-        					'priority' => 90,
-        					'title'	=> 'Redirects',
-        					'icon'	=> 'fa fa-external-link',
-        					'is_root' => false,
-        					'tree'	=> $root,
-							'base' => '/admin/redirect/',
-        			)
-        	);
-        	
+    public function onSystemRebuildMenu($event)
+    {
+        if ($model = $event->getArgument('model'))
+        {
+            $root = $event->getArgument('root');
+            $redirect = clone $model;
+            
+            $redirect->insert(array(
+                'type' => 'admin.nav',
+                'priority' => 90,
+                'title' => 'Redirects',
+                'icon' => 'fa fa-external-link',
+                'is_root' => false,
+                'tree' => $root,
+                'base' => '/admin/redirect/'
+            ));
+            
             $children = array(
-                    array( 'title'=>'List', 'route'=>'/admin/redirect/routes', 'icon'=>'fa fa-list' ),
-                    array( 'title'=>'Settings', 'route'=>'/admin/redirect/settings', 'icon'=>'fa fa-cogs' ),
+                array(
+                    'title' => 'List',
+                    'route' => '/admin/redirect/routes',
+                    'icon' => 'fa fa-list'
+                ),
+                array(
+                    'title' => 'Settings',
+                    'route' => '/admin/redirect/settings',
+                    'icon' => 'fa fa-cogs'
+                )
             );
-           	$redirect->addChildrenItems( $children, $root );
+            $redirect->addChildren($children, $root);
             
             \Dsc\System::instance()->addMessage('Routes Manager added its admin menu items.');
         }
