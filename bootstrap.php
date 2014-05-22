8f64036b6c51d4c9c712d685d94ad41983417025
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-05-22 16:19:57 -0400

    added a listener event to the OnERROR event, and put all logic for redirect handler into a reusable method, fixes #3, fixes #2

--- bootstrap.php
+++ bootstrap.php
@@ -1,89 +1,50 @@
 <?php
+
 class RedirectBootstrap extends \Dsc\Bootstrap
 {
+
     protected $dir = __DIR__;
+
     protected $namespace = 'Redirect';
 
-    protected function preAdmin(){
-    	parent::preAdmin();
-    	 
-    	\Dsc\Apps::registerPath( $this->dir . "/src/Redirect/MassUpdate", 'massupdate' );
+    protected function preAdmin()
+    {
+        parent::preAdmin();
+        
+        \Dsc\Apps::registerPath($this->dir . "/src/Redirect/MassUpdate", 'massupdate');
     }
-    
+
     protected function runSite()
     {
         $f3 = \Base::instance();
-        $f3->set( 'ONERROR', function ( $f3 )
+        $f3->set('ONERROR', function ($f3)
         {
-            if ($f3->get( 'ERROR.code' ) == '404')
+            $response = \Redirect\Factory::handleError();
+            
+            // Run the response through a Listener event
+            $event = \Dsc\System::instance()->trigger( 'onError', array(
+            	'response' => $response
+            ) );
+            
+            $response = $event->getArgument('response');
+            
+            switch ($response->action)
             {
-                $model = new \Redirect\Admin\Models\Routes;
-
-                // lets find a proper redirection, if exists
-                $path = substr( $model->inputFilter()->clean( $f3->hive()['PATH'], 'string' ), 1 );
-
-                $redirect = null;
-                if ($route = $model->setState( 'filter.url.alias', $path )->getItem()) 
-                {
-                    // count the number of times a redirect is hit and track the date of the last hit
-                    $route->hits = (int) $route->hits + 1;
-                    $route->last_hit = \Dsc\Mongo\Metastamp::getDate('now');
-                    $route->save();
-                                        
-                    $redirect = trim( $route->{'url.redirect'} );                	
-                }
-                // no defined redirect for this $path was found, so create a record in the redirect manager
-                else 
-                {
-                	// TODO add a config option to disable this
-                    $new_route = new \Redirect\Admin\Models\Routes;
-                    $new_route->set('url.alias', $path);
-                    $new_route->hits = 1;
-                    $new_route->last_hit = \Dsc\Mongo\Metastamp::getDate('now');
-                    try {
-                        $new_route->save();
-                    } catch (\Exception $e) {
-                        // couldn't save for some reason
-                        // add it to Logger?                    	
-                    }
-                }
-                
-                // don't redirect if the path and redirect are the same to prevent recursion
-                if (!empty( $redirect ) && $path != $redirect)
-                {
-                    // if the string doesn't begin with /, make sure it does
-                    if (strpos($redirect, '/') !== 0) {
-                        $redirect = '/' . $redirect;
-                    }                    
-                    $f3->reroute( $redirect );
-                }
-                else
-                { 
-                    // use default error route specified by user in Redirect::Settings
-                    $settings = \Redirect\Admin\Models\Settings::fetch();
-                    $default_redirect = $settings->{'general.default_error_404'};
-                    if (!empty($default_redirect) && $path != $default_redirect)
-                    {
-                        // if the string doesn't begin with /, make sure it does
-                        if (strpos($default_redirect, '/') !== 0) {
-                            $default_redirect = '/' . $default_redirect;
-                        }
-                        
-                        $f3->reroute( $default_redirect );
-                    }
-                    else
-                    {
-                        // let f3 default error handler handle it since the user didn't specify a default 404 redirect
-                        return false;
-                    }
-                }
+            	case "handle":
+            	    $f3->call($response->callable_handle);
+            	    break;
+            	case "html":
+            	    echo $response->html;
+            	    break;
+            	case "redirect":
+            	    $f3->reroute( $response->redirect_route );
+            	    break;
+            	default:
+            	    // by returning false, let f3 default error handler handle the error
+            	    return false;
+            	    break;
             }
-            else
-            {
-                // let f3 default error handler handle all other error types
-                return false;
-            }    
-        } );
+        });
     }
 }
 $app = new RedirectBootstrap();
---
+++ src/Redirect/Factory.php
@@ -0,0 +1,93 @@
+<?php
+namespace Redirect;
+
+class Factory extends \Dsc\Singleton
+{
+
+    public static function handleError()
+    {
+        $response = new \stdClass;
+        $response->action = null; 
+        $response->redirect_route = null;
+        $response->callable_handle = null;
+        $response->html = null;
+        
+        $f3 = \Base::instance();
+        if ($f3->get('ERROR.code') == '404')
+        {
+            $model = new \Redirect\Admin\Models\Routes();
+            
+            // lets find a proper redirection, if exists
+            $path = substr($model->inputFilter()->clean($f3->hive()['PATH'], 'string'), 1);
+            
+            $redirect = null;
+            if ($route = $model->setState('filter.url.alias', $path)->getItem())
+            {
+                // count the number of times a redirect is hit and track the date of the last hit
+                $route->hits = (int) $route->hits + 1;
+                $route->last_hit = \Dsc\Mongo\Metastamp::getDate('now');
+                $route->save();
+                
+                $redirect = trim($route->{'url.redirect'});
+            }
+            // no defined redirect for this $path was found, so create a record in the redirect manager
+            else
+            {
+                // TODO add a config option to disable this
+                $new_route = new \Redirect\Admin\Models\Routes();
+                $new_route->set('url.alias', $path);
+                $new_route->hits = 1;
+                $new_route->last_hit = \Dsc\Mongo\Metastamp::getDate('now');
+                try
+                {
+                    $new_route->save();
+                }
+                catch (\Exception $e)
+                {
+                    // couldn't save for some reason
+                    // add it to Logger?
+                }
+            }
+            
+            // don't redirect if the path and redirect are the same to prevent recursion
+            if (!empty($redirect) && $path != $redirect)
+            {
+                // if the string doesn't begin with /, make sure it does
+                if (strpos($redirect, '/') !== 0)
+                {
+                    $redirect = '/' . $redirect;
+                }
+                $response->action = 'redirect';
+                $response->redirect_route = $redirect;                
+            }
+            else
+            {
+                // use default error route specified by user in Redirect::Settings
+                $settings = \Redirect\Admin\Models\Settings::fetch();
+                $default_redirect = $settings->{'general.default_error_404'};
+                if (!empty($default_redirect) && $path != $default_redirect)
+                {
+                    // if the string doesn't begin with /, make sure it does
+                    if (strpos($default_redirect, '/') !== 0)
+                    {
+                        $default_redirect = '/' . $default_redirect;
+                    }
+                    $response->action = 'redirect';
+                    $response->redirect_route = $default_redirect;
+                }
+                else
+                {
+                    // let f3 default error handler handle it since the user didn't specify a default 404 redirect
+                    $response->action = false;
+                }
+            }
+        }
+        else
+        {
+            // let f3 default error handler handle all other error types
+            $response->action = false;
+        }
+        
+        return $response;
+    }
+}
